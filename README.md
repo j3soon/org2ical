08c796f274ade1fd5b2d609fc515763b1aaa4b87
Johnson <j3.soon@msa.hinet.net>
2022-11-14 23:10:43 +0800

    Add package info

---
+++ README.md
@@ -0,0 +1,5 @@
+# org2ical
+
+Generate a ([iCalendar](https://icalendar.org/)) (.ics) file based on a ([OrgMode](https://orgmode.org/)) (.org) file.
+
+This package is especially useful if your OrgMode file is too large to be exported with [OrgMode iCalendar Export](https://orgmode.org/manual/iCalendar-Export.html).
---
+++ setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+github_url = "https://github.com/j3soon/org2ical"
+
+setuptools.setup(
+    name="org2ical",
+    version="0.0.1",
+    author="Johnson",
+    author_email="j3.soon777@gmail.com",
+    description="Generate a iCalendar (.ics) file based on a OrgMode (.org) file.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=github_url,
+    project_urls={
+        "Issues": f"{github_url}/issues",
+        "Source Code": github_url,
+    },
+    keywords=(
+        "package, parser, converter, python, orgmode, icalendar"
+    ),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where=".", exclude=("tests*",)),
+    install_requires=[
+        # TODO: Add orgparse after merged
+    ],
+    extras_require={
+        "testing": ["pytest", "mypy", "flake8", "pylint", "icalendar"],
+    },
+)
+
+# Note: PyPI seems to only recognize double-quoted strings
