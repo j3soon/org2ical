52cb52428a7ecc165fd9e1a914ac2bc2f82fd0cd
Johnson <j3.soon@msa.hinet.net>
2022-11-18 18:39:36 +0800

    Bump version number to v0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ github_url = "https://github.com/j3soon/org2ical"
 
 setuptools.setup(
     name="org2ical",
-    version="0.0.1",
+    version="0.0.2",
     author="Johnson",
     author_email="j3.soon777@gmail.com",
     description="Generate a iCalendar (.ics) file based on a OrgMode (.org) file.",
